fad9f2e61176ca929b0532b9021eb6ec031c548a
devdhaif <devdhaif@gmail.com>
2024-07-21 01:14:07 +0300

    ghp

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  basePath: "/next-food",
+  output: "export",
+  reactStrictMode: true,
+};
 
 export default nextConfig;
